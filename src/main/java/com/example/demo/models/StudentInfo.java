3bf8b69531020157a13a88b399190c6d51430776
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2025-05-22 16:55:28 +0200

    correcciones anotaciones

--- src/main/java/com/example/demo/models/StudentInfo.java
+++ src/main/java/com/example/demo/models/StudentInfo.java
@@ -1,6 +1,9 @@
 package com.example.demo.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class StudentInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
